616a9af55eb8dcb3ee30483d6df68d363257db1f
Piotr Goryl <piotr.goryl@3-controls.com>
2016-12-21 11:14:48 +0100

    Incorporated Thomas's sources

--- source/manual/ds_writing/cpp_exchanging/ConstDevArray.cpp
+++
@@ -1,8 +0,0 @@
-Tango::DevVarLongArray *MyDev::dev_array()
-{
-	Tango::DevVarLongArray	*argout;
-		
-	long output_array_length = ...;
-	argout = create_DevVarLongArray(buffer,output_array_length);
-	return argout;
-}
--- source/manual/ds_writing/writing/steppermotorname.cpp
+++
@@ -1,5 +0,0 @@
-void StepperMotorClass::device_name_factory(vector<string> &list)
-{
-	list.push_back("sr/cav-tuner/1");
-	list.push_back("sr/cav-tuner/2");
-}
