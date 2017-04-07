cf1435a725acc7580b52e19411c26c4159c4e001
Piotr Goryl <piotr.goryl@3-controls.com>
2017-04-07 14:15:00 +0200

    TD-61 Checking if master_doc pointing to contents influence PDF contetn

--- source/conf.py
+++ source/conf.py
@@ -58,7 +58,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'contents'
 
 # General information about the project.
 project = u'Tango Controls'
