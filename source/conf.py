1d0ea08007b3144e7b0e21635f6418bd861516f5
Piotr Goryl <piotr.goryl@3-controls.com>
2017-05-26 10:13:56 +0200

    progress #2 - theme name changed again in conf.py - there are some issues on the rtd

--- source/conf.py
+++ source/conf.py
@@ -181,7 +181,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'tango_cs_rtd_theme'
+html_theme = 'tango-cs-rtd-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
