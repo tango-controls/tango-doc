dd277c95b13feeeaa0ee09e2042f61fcd013aa79
Thomas Braun <thomas.braun@byte-physics.de>
2020-10-26 20:14:05 +0100

    source/conf.py: Raise copyright year

--- source/conf.py
+++ source/conf.py
@@ -68,7 +68,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'Tango Controls'
-copyright = u'2017-2019, Tango Community, Creative Commons Attribution 4.0 International (CC BY 4.0)'
+copyright = u'2017-2020, Tango Community, Creative Commons Attribution 4.0 International (CC BY 4.0)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -380,7 +380,7 @@ texinfo_documents = [
 epub_title = u'Tango Controls Documentation'
 epub_author = u'Tango Community'
 epub_publisher = u'Tango Community'
-epub_copyright = u'2017-2019, Tango Community, CC BY 4.0'
+epub_copyright = u'2017-2020, Tango Community, CC BY 4.0'
 
 # The basename for the epub file. It defaults to the project name.
 #epub_basename = u'Tango Controls Documentation'
